080a00f6cfe8dbc58510d33a957c4c979ab7fc7c
Sharad Kumar Singh <singhsharad529@gmail.com>
2025-03-31 13:19:47 +0530

    setup env variables

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import express, { NextFunction } from "express";
+import dotenv from "dotenv";
 import { Request, Response } from "express";
 import CustomError from "./utils/customError.js";
 import { errorHandler } from "./middleware/errorHandler.js";
 
+dotenv.config();
 const app = express();
 
 app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Working with types and watch command");
@@ -21,6 +25,6 @@ app.all("*", (req: Request, res: Response, next: NextFunction) => {
 
 app.use(errorHandler);
 
-app.listen(5000, () => {
-  console.log("Express Server is listening on PORT 5000");
+app.listen(PORT, () => {
+  console.log("Node Server is listening on PORT " + PORT);
 });
